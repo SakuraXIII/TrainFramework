fa46caea330f045536edecf4f9cffb4b4090b2f4
SakuHx <1162617171@qq.com>
2024-07-07 01:43:01 +0800

    优化标签加载

--- datasets.py
+++ datasets.py
@@ -183,3 +183,28 @@ class ImgDataset(Dataset):
             image = self.transform(image)
 
         return image
+
+
+class ImageNet(Dataset):
+    def __init__(self, root, transform=None, image_set: str = ""):
+        super().__init__()
+        self.data_dir = root
+        self.transform = transform
+        self.image_set = image_set
+        self.image_files = Path(root, "Data", "CLS-LOC")
+        self.image_idx = Path(root, "ImageSets", "CLS-LOC")
+        with open(self.image_idx / f"{image_set}.txt", 'r') as f:
+            lines = f.readlines()
+            self.image_path = [line.strip() for line in lines]
+
+    def __len__(self):
+        return len(self.image_path)
+
+    def __getitem__(self, idx):
+        image_path, label_id = self.image_path[idx].split(" ")
+        image = Image.open(self.image_files / self.image_set / image_path).convert("RGB")
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, int(label_id)
