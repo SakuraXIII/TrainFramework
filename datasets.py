245fe8e1d18c84e6e8cba7f6634a0ca8c94f4c93
SakuHx <1162617171@qq.com>
2024-07-09 18:12:23 +0800

    增加 COCO 数据集载入

--- datasets.py
+++ datasets.py
@@ -208,3 +208,38 @@ class ImageNet(Dataset):
             image = self.transform(image)
 
         return image, int(label_id)
+
+
+class MSCOCO(Dataset):
+    def __init__(self, root, transform, img_list=None):
+        assert root[-1] == '/', "root to COCO dataset should end with \'/\', not {}.".format(
+            root)
+
+        if img_list:
+            self.image_paths = []
+            with open(img_list, 'r') as r:
+                lines = r.read().splitlines()
+                for line in lines:
+                    self.image_paths.append(root + line)
+        else:
+            self.image_paths = sorted(glob(root + "*.jpg"))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            object: image.
+        """
+        img_path = self.image_paths[index]
+
+        img = Image.open(img_path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img
+
+    def __len__(self):
+        return len(self.image_paths)
